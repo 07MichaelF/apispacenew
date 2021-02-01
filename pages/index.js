77ad1f12cc9298389ecc2c856a6864bf31df0703
Michael Figueroa <59900093+07MichaelF@users.noreply.github.com>
2021-02-01 12:53:23 -0500

    Consumo de una API con react next.js

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,65 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  /* */
+  const [data, setData] = useState();
+  useEffect(() => {
+    fetch("https://test.spaceflightnewsapi.net/api/v2/articles", {
+      method: "GET",
+    }).then(async (resp) => {
+      const datos = await resp.json();
+      console.log("Data:", datos);
+      setData(datos);
+    });
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title> Spaceflight News </title> <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://spaceflightnewsapi.net"> Spaceflight News! </a>{" "}
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {data &&
+            data.map((result, i) => {
+              i++;
+              let summa = result.summary;
+              summa = summa.split(" ");
+              let summary = "";
+              for(let i=0; i<=21;i++){
+                summary += " "+ summa[i]
+              }
+              console.log(summa);
+              return (
+                <a
+                  key={i}
+                  href={result.url}
+                  className={styles.card}
+                  target="blank"
+                >
+                  <img id="imgUrl" src={result.imageUrl}></img>
+                  <h3>
+                    {i} {result.title}{" "}
+                  </h3>
+                  <p className="descr">{summary}...</p>
+                </a>
+              );
+            })}
         </div>
       </main>
-
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <p>
+        Powered by
+          <strong> Michael Figueroa</strong> {" "}
+        </p>
+        
+      </footer>{" "}
     </div>
-  )
+  );
 }
